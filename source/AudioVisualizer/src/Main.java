d83edb4c2c7c649f518270d9c67604bf14157189
Rahul Rameshbabu <rahulrameshbabu16@mittymonarch.com>
2015-11-08 20:07:15 -0800

    Forgot to open Mic

--- source/AudioVisualizer/src/Main.java
+++ source/AudioVisualizer/src/Main.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         MicrophoneAnalyzer analyzer = new MicrophoneAnalyzer(AudioFileFormat.Type.WAVE);
-        Scanner input = new Scanner(System.in);
-        while(!input.nextLine().equals("e")) {
+        analyzer.open();
+        while(true) {
             System.out.println(analyzer.getAudioVolume());
             System.out.println(analyzer.getFrequency());
             //You can even store and write the audio with this API
